661483d9ab4657057cf89b191c735d949fb2feb9
Emily Tompsett <etompsett@mka.org>
2023-11-06 09:21:38 -0500

    Add files via upload

--- BST.java
+++ BST.java
@@ -46,7 +46,23 @@ public class BST {
 	}
 
 	public boolean add(int item){
-	
+		// cannot have a null root elt - will always create node w/
+		if(item < this.element) {
+			if (left != null) {
+				// recurse on left subtree
+				return left.add(item);
+			}
+			left = new BST(item);
+			return true;
+		}
+		else if(item > this.element) {
+			if ( right != null) {
+				return right.add(item);
+			}
+			right = new BST(item);
+			return true;
+		}
+		return false;
 	} 
 	
 	public boolean remove(int item) { // do not implement yet
@@ -57,14 +73,21 @@ public class BST {
 	private void visit() { System.out.print(element + " "); }
 	
 	public void inOrder() { // visits left, parent, right
-
+		if (left !=null) { left.inOrder(); }
+		this.visit();
+		if (right!=null) { right.inOrder(); }
 	}
+	
 	public void preOrder() { // parent, left, right
-
+		this.visit();
+		if (left != null) { left.preOrder(); }
+		if (right!= null) { right.preOrder(); }
 	}
 
 	public void postOrder() { // left, right, parent
-
+		if (left != null) { left.postOrder(); }
+		if (right!= null) { right.postOrder(); }
+		this.visit();
 	}
 	
 	public void breadthFirst() {
@@ -72,6 +95,11 @@ public class BST {
 	}
 	
 	public static void main(String[] arg) {
-	
+		BST test = new BST(10);
+		test.add(5);
+		test.add(35);
+		test.add(7);
+		test.add(10);
+		test.postOrder();
 	}
 }
