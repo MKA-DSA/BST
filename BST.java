f18b46deb78c6335ff6417c2152cf16c09083de9
Emily Tompsett <etompsett@mka.org>
2024-12-05 14:19:27 -0500

    Add files via upload

--- BST.java
+++ BST.java
@@ -1,5 +1,4 @@
 /* DSA
- * BST Fall 2023
  */
 import java.util.*;
 
@@ -34,137 +33,75 @@ public class BST {
 	
 	// methods
 	public boolean contains(int item) { 
-		if(this.element == item) return true;
-		if(item < this.element) {
-			if(this.left == null) return false;
-			return this.left.contains(item);
+		if (element == item) {
+			return true;
 		}
-		if(this.right == null) return false;
-		return this.right.contains(item);
+		if (item > element && this.getRight() != null) {
+			return this.getRight().contains(item);
+		}
+		if (this.getLeft() != null) {
+			return this.getLeft().contains(item);
+		}
+		return false;
 	} 
 	
 	public BST findMin() { 
-		if(this.left != null) return this.left.findMin();
-		return this;
+		
 	}
 
 	public BST findMax() { 
-		if(this.right != null) return this.right.findMax();
-		return this;
+		
 	}
-	
-	private BST findMaxParent() { // never going to call on a BST w/ no right
-		if (this.right.getRight() == null) {
-			return this;
-		}
-		return this.right.findMaxParent();
-	}
-	
 
 	public boolean add(int item){
-		// cannot have a null root elt - will always create node w/
-		if(item < this.element) {
-			if (left != null) {
-				// recurse on left subtree
-				return left.add(item);
-			}
-			left = new BST(item);
-			return true;
-		}
-		else if(item > this.element) {
-			if ( right != null) {
-				return right.add(item);
-			}
-			right = new BST(item);
-			return true;
-		}
-		return false;
+		 if (element == item) {
+			 return false;
+		 } 
+		 if (element > item) {
+			 if (this.getLeft() == null) {
+				 this.setLeft(new BST(item));
+				 return true;
+			 }
+			 return this.getLeft().add(item);
+		 }
+		 else {
+			 if (this.getRight() == null) {
+				 this.setRight(new BST(item));
+				 return true;
+			 }
+			 return this.getRight().add(item); 
+		 }
 	} 
 	
 	public boolean remove(int item) { // do not implement yet
-		// trying to remove root, doesn't have children so can't remove
-		if (this.left == null && this.right == null) return false;//it has no children
-		return this.remove(item, null, false);
+
 	}
 	
-	private boolean remove(int item, BST parent, boolean isLeft) {
-		if (this.element == item) { // found the thing looking for
-			// has no children, remove from parent (know that it isn't the root)
-			if (right == null && left == null) {
-				if (isLeft)  parent.setLeft(null);
-				else  parent.setRight(null);
-				return true;
-			}
-			// has one child, replace current node with child
-			if (left == null) { //current node only has a right child
-				if (isLeft)  parent.setLeft(right);
-				else  parent.setRight(right);
-				return true;
-			}
-			if (right == null) {
-				if (isLeft)  parent.setLeft(left);
-				else  parent.setRight(left);
-				return true;
-			}
-			// has two children - get largest on left swap, recurse
-			// findMaxParent()
-			BST maxParent;
-			BST toSwap;
-			boolean toSwapisLeft = false;
-			if (this.left.getRight() == null) {
-				maxParent = this;
-				toSwap = this.left;
-				toSwapisLeft = true;
-			}
-			else {
-				maxParent = this.left.findMaxParent();
-				toSwap = maxParent.getRight();
-			}
-			int temp = toSwap.getElement();
-			toSwap.setElement(element);
-			element = temp; 
-			return toSwap.remove(item, maxParent, toSwapisLeft);
-		}
-		// removing something other than current node
-		if (item < this.element) {
-			if (this.left != null) return this.left.remove(item, this, true);
-			return false;
-		}
-		if (this.right != null) return this.right.remove(item, this, false);
-		return false;
-	}
+	
 
 	// traversals
 	private void visit() { System.out.print(element + " "); }
 	
 	public void inOrder() { // visits left, parent, right
-		if (left !=null) { left.inOrder(); }
+		if (this.getLeft() != null) this.getLeft().inOrder();
 		this.visit();
-		if (right!=null) { right.inOrder(); }
+		if (this.getRight() != null) this.getRight().inOrder();
 	}
 	
 	public void preOrder() { // parent, left, right
 		this.visit();
-		if (left != null) { left.preOrder(); }
-		if (right!= null) { right.preOrder(); }
+		if (this.getLeft() != null) this.getLeft().inOrder();
+		if (this.getRight() != null) this.getRight().inOrder();
 	}
 
 	public void postOrder() { // left, right, parent
-		if (left != null) { left.postOrder(); }
-		if (right!= null) { right.postOrder(); }
+		if (this.getLeft() != null) this.getLeft().inOrder();
+		if (this.getRight() != null) this.getRight().inOrder();
 		this.visit();
 	}
 	
 	public void breadthFirst() {
-		//LinkedQueue 
-		LinkedQueue<BST> Q = new LinkedQueue<BST>();
-		Q.enqueue(this);
-		while (!Q.isEmpty()) {
-			BST current = Q.dequeue();
-			if (current.getLeft() != null) Q.enqueue(current.getLeft());
-			if (current.getRight() != null) Q.enqueue(current.getRight());
-			current.visit();
-		}
+		
 		
 	}
 	
@@ -174,16 +111,34 @@ public class BST {
 		test.add(35);
 		test.add(7);
 		test.add(6);
-		test.add(11);
-		test.add(2);
+		System.out.println();
+		System.out.print("pre order: ");
+		
+		test.preOrder();
+		System.out.println();
+
+		System.out.print("in order: ");
+		test.inOrder();
+		System.out.println();
+
+		System.out.print("post order: ");
+		test.postOrder();
+		
+		
+
+	/*	test.add(2);
 		test.add(4);
 		test.add(3);
-
-		test.preOrder();
-		test.remove(11);
+		
 		System.out.println();
+		System.out.print("pre order: ");
+		
 		test.preOrder();
-		System.out.println(test.contains(11));
-		System.out.println(test.contains(3));
+		System.out.println();
+
+		System.out.print("in order: ");
+		test.inOrder();*/
+	//	System.out.println(test.contains(11));
+		//System.out.println(test.contains(3));
 	}
 }
